c755fbfdb8104148433cfbf66400e119923b8b17
JatinLohia013 <jatinlohia2005@gmail.com>
2025-10-28 23:22:35 +0530

    Task1 Completed

---
+++ Task1.py
@@ -0,0 +1,12 @@
+first=eval(input("Enter first number: "))
+second=eval(input("Enter second number: "))
+
+sum=first+second
+subtraction=first-second
+multiplication=first*second
+division=first/second
+
+print("Addition: ",sum)
+print("Subtraction: ",subtraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
